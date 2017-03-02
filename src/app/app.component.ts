2bf0aba3a0c72bce3eb98c630a00fa75996ebf3c
Jennifer Currie <renestl@gmail.com>
2017-03-02 12:45:58 -0600

    moved title, added styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,6 @@ export class MarkdownComponent {
 })
 
 export class AppComponent {
-  title = 'Markdown Previewer';
   userInput: MarkdownComponent = {
     text: 'Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`,~~strikethrough~~ .\n\nShopping list:\n* apples\n* oranges\n* pears\n\nNumbered list:\n1. apples\n2. oranges\n3. pears\n\n*[Jennifer Currie](http://freecodecamp.com/renestl)*'
   };
