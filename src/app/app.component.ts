750f3105b703555560057abd1faf673449b6814c
Jennifer Currie <renestl@gmail.com>
2017-03-01 21:15:34 -0600

    completed binding text area and display div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
-export class TextEntered {
-  text: string;
+
+export class MarkdownComponent {
+
+   text: string;
+
 }
 
 @Component({
@@ -11,8 +14,8 @@ export class TextEntered {
 })
 export class AppComponent {
   title = 'Markdown Previewer';
-  userInput: TextEntered = {
-    text: "Testing it out"
+  userInput: MarkdownComponent = {
+    text: 'Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`,~~strikethrough~~ .\n\nShopping list:\n* apples\n* oranges\n* pears\n\nNumbered list:\n1. apples\n2. oranges\n3. pears\n\n*[Jennifer Currie]()*'
   };
 
 }
